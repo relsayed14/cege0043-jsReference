7e11a32705f3a968ce9311c376fbc6e391e69f36
Reem Elsayed <reem.elsayed.14@ucl.ac.uk>
2019-02-27 21:16:39 -0500

    Adapted AJAX code to process NodeJS Change

--- AJAX.js
+++ AJAX.js
@@ -1,21 +1,28 @@
 //define the globl variabel to process the AJAX request
 var xhrNode; 
 
-function callDivChange() {
-		xhr = new XMLHttpRequest();
-        xhr.open("GET", "test.html", true);
-        xhr.onreadystatechange = processDivChange;
-        xhr.send();
+function callDivNodeJSChange() {
+		xhrNode = new XMLHttpRequest();
+        var url = "http://developer.cege.ucl.ac.uk: " +httpPortNumber;
+        xhrNode.open("GET", url, true);
+        xhrNode.onreadystatechange = processDivNodeJSChange;
+        try {
+        	xhrNode.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        }
+        catch (e) {
+        	//this only works in internet explorer
+        }
+        xhrNode.send();
 }
 
-function processDivChange() {
+function processDivNodeJSChange() {
 	// while waiting for response from server
-	if (xhr.readyState < 4) {
-		document.getElementById('div1').innerHTML = "Loading...";
-	} else if (xhr.readyState === 4) 	// 4 = response from server has been completely loaded
+	if (xhrNode.readyState < 4) {
+		document.getElementById('ajaxtest').innerHTML = "Loading...";
+	} else if (xhrNode.readyState === 4) 	// 4 = response from server has been completely loaded
 {
-	if (xhr.status == 200 && xhr.status < 300)  {
+	if (xhrNode.status == 200 && xhr.status < 300)  {
 		// http status between 200 to 299 are all successful
-		document.getElementById('div1').innerHTML = xhr.responseText;
+		document.getElementById('ajaxtest').innerHTML = xhrNode.responseText;
 	}
 }
