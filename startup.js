27162b6caa2733058c97273923f78abab10b8c7c
Reem Elsayed <reem.elsayed.14@ucl.ac.uk>
2019-03-13 14:59:23 -0400

    startup functions for quiz and questions apps

--- startup.js
+++ startup.js
@@ -11,3 +11,24 @@ function startup() {
 		trackAndCircle();
 	}, false);
 }
+
+function questionStartup() {
+	alert("Welcome to the Questions App!");
+	document.addEventListener('DOMConentLoaded', function() {
+		getPort(); // obtain port number 
+		loadW3HTML(); // add HTML functionality
+		trackLocation(); // track the user's location
+	},false);
+}
+
+function quizStartup() {
+	alert("Welcome to the Quiz App!");
+	document.addEventListener('DOMConentLoaded', function() {
+		getPort(); // obtain port number 
+		loadW3HTML(); // add HTML functionality
+		trackLocation(); // track the user's location
+		// add code here to zoom into the area where user is to make it easier to choose quiz point?
+		// check code sources 
+
+	},false);
+}
