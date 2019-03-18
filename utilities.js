fa739233e9bab81dec857087bd4b7cf52f9ebb03
Reem Elsayed <reem.elsayed.14@ucl.ac.uk>
2019-03-18 19:22:31 -0400

    final changes

--- utilities.js
+++ utilities.js
@@ -1,15 +1,15 @@
 	//define global variables to store port numbers
 
-window.httpPortNumber=0;
-window.httpsPortNumber=0;
+	var httpPortNumber;
+	var httpsPortNumber;
 
 	function getPort () {
 		var xhr = new XMLHttpRequest();
 		xhr.addEventListener("load",function() {
 			var parser = new DOMParser();
 			var doc = parser.parseFromString(xhr.responseText, "application/xml");
-        	window.httpPortNumber= doc.getElementsByTagName("node-port-http").item(0).textContent;
-       		window.httpsPortNumber= doc.getElementsByTagName("node-port-https").item(0).textContent;
+        	httpPortNumber= doc.getElementsByTagName("node-port-http").item(0).textContent;
+       		httpsPortNumber= doc.getElementsByTagName("node-port-https").item(0).textContent;
         console.log("Port : " + httpPortNumber);
 	});
 
